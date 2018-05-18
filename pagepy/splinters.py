0f507aa633f29febf16986acb62fb41d5ae5ae09
hamogu <hgunther@mit.edu>
2018-05-18 15:19:13 -0400

    Tuesday evening event

--- pagepy/splinters.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-
-def data(**kwargs):
-    with open('data/splinters.csv') as f:
-        r = csv.DictReader(f, delimiter=";")
-        splin = [row for row in r]
-
-    return {'splinters': splin}
